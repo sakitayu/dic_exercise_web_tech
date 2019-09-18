d8bae372548f6bc51fcaf3d5446a72f885b764a9
Yu Sakita <sakitakitakita@gmail.com>
2019-09-18 11:31:05 +0900

    Web技術入門最初のコミット

---
+++ test.rb
@@ -0,0 +1,30 @@
+require 'webrick'
+
+module WEBrick
+  module HTTPServlet
+    FileHandler.add_handler('rb', CGIHandler)
+  end
+end
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+# WEBrick::HTTPServlet::FileHandlerをWEBrick::HTTPServlet::ERBHandlerに変更する
+# '（DocumentRootの値）/'を'index.html.erb'に変更する
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'index.html.erb')
+
+# 'test.html'を'test.html.erb'に変更する (課題用)
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+
+# この一行を追記
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+server.start
